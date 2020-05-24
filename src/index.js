ae6a8bffb6fe92fe6143deb0f0fc326d5f13dcc5
binod <kbinod987@gmail.com>
2020-05-25 00:29:10 +0530

    add file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-ReactDOM.render(<h1>hello world!!</h1>, document.getElementById("root"));
+import React from "react"; //jsx use karne ke liye html tag
+import ReactDOM from "react-dom"; //render method run ke liye
+ReactDOM.render(<h1>hello world!!</h1>, document.getElementById("root"));//kya dikhana h or kha dikha na h
